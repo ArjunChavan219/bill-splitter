019db0e183a2418a86ea03833a408f8f1c8fcf95
Arjun Chavan <arjun.chavan@pens.com>
2023-02-18 20:06:01 +0530

    Homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAuth } from "../provider/AuthProvider"
 
-import { LoginDiv, SignUp } from "./Modals"
-import Login from "./Modals/Login"
+import "../styles/Home.css"
 
 
 function Home() {
-    const { user } = useAuth()
+    const [error, setError] = useState("")
+    const { user, login, server } = useAuth()
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -16,15 +16,42 @@ function Home() {
         }
     }, [])
 
+    function handleLogin(event) {
+        event.preventDefault()
+        const username = event.target.elements[0].value
+        const password = event.target.elements[1].value
+        
+        server.login(username, password).then(data => {
+            if (data.success) {
+                login(username)
+            } else {
+                setError(data.error)
+            }
+            console.log(data.error);
+        })
+    }
+
+    function onExit() {
+        setError("")
+    }
+
     return (
         <>
-            <br />
-            <p>Logo</p>
-            <div>Home Page</div>
-            {!user.username && <Login />}
-            {/* {!user.username && <LoginDiv />}
-            {!user.username && <SignUp />} */}
-            <div>Test data</div>
+            <div className={"container"}>
+                <div className={"top"}></div>
+                <div className={"bottom"}></div>
+                <div className={"center"}>
+                    <h2>Please Sign In</h2>
+                    <form id="login" onSubmit={handleLogin}>
+                        <input type="username" placeholder="Email" autoComplete="username" className={error === "Username" ? "error" : "normal"} onBlur={onExit}/>
+                        <input type="password" placeholder="Password" autoComplete="current-password" className={error === "Password" ? "error" : "normal"} onBlur={onExit}/>
+                        <button type="submit" className={"button"}>
+                            Login
+                        </button>
+                    </form>
+                    <h2>&nbsp;</h2>
+                </div>
+            </div>
         </>
     )
 }
